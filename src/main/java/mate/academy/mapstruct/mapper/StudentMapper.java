e3a29ef5795cd2affdc9e60e3dddf2625f063ee2
Vlad <arhipov09090az@gmail.com>
2026-09-21 20:39:58 +0300

    implemented

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,14 +1,55 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
+import java.util.Optional;
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
+import mate.academy.mapstruct.model.Group;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.Named;
 
+@Mapper(config = MapperConfig.class)
 public interface StudentMapper {
+
+    @Mappings({
+            @Mapping(source = "group.id", target = "groupId"),
+            @Mapping(source = "subjects", target = "subjectIds"),
+    })
     StudentDto toDto(Student student);
 
+    @Mapping(source = "group.id", target = "groupId")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mappings({
+            @Mapping(source = "groupId", target = "group", qualifiedByName = "groupById"),
+            @Mapping(source = "subjects", target = "subjects", qualifiedByName = "subjectsById"),
+    })
     Student toModel(CreateStudentRequestDto requestDto);
+
+    @Named("groupById")
+    default Group groupById(Long id) {
+        return Optional.ofNullable(id)
+                .map(Group::new)
+                .orElse(null);
+    }
+
+    @Named("subjectsById")
+    default List<Subject> subjectsById(List<Long> ids) {
+        if (ids == null) {
+            return null;
+        }
+        return ids.stream()
+                .map(Subject::new)
+                .toList();
+    }
+
+    default Long subjectToId(Subject subject) {
+        return subject == null ? null : subject.getId();
+    }
 }
